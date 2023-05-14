0838bf5b3c07ab63180e45b81ba073000a63b4c2
avelos442 <vika.00v@mail.ru>
2023-05-15 00:59:16 +0300

    commit

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Declaration
 
-# Create your views here.
+
+def home_view(request):
+    qs = Declaration.objects.all()
+    return render(request, 'scraping/home.html', {'object_list': qs})
+
